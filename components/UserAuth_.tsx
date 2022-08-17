7e2a8ad5c110124dc51d645dace7921fe28588ec
LwaziNF <lwazinf@gmail.com>
2022-08-17 21:24:47 +0200

    basic :: working on auth, refresh function

--- components/UserAuth_.tsx
+++ components/UserAuth_.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Router } from "react-router-dom";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRecoilState } from "recoil";
@@ -10,10 +10,10 @@ import { db } from "./services/firebase";
 interface UserAuth_Props {}
 
 const UserAuth_ = ({}: UserAuth_Props) => {
-  const navigate = useNavigate();
-  const refreshPage = () => {
-    navigate(0);
-  };
+  // const navigate = useNavigate();
+  // const refreshPage = () => {
+  //   navigate(0);
+  // };
 
   // 👇️👇️👇️ (string) Recoil addressState is used when a purchase is made..
   const [address_, setAddress_] = useRecoilState(addressState);
@@ -80,45 +80,47 @@ const UserAuth_ = ({}: UserAuth_Props) => {
     checkIfWalletIsConnected();
   }, []);
   return (
-    <div className={`w-full h-[50px] fixed top-0`}>
-      {/* 👇️👇️👇️ MetaMask button.. */}
-      <div
-        className={`transition-all duration-200 opacity-80 hover:opacity-100 flex flex-row relative right-3 w-[150px] h-full items-center justify-center m-2 ml-auto cursor-pointer`}
-        onClick={() => {
-          connectWallet();
-        }}
-      >
-        <div className={`w-[40px] h-full transition-all duration-500`}>
-          <img
-            src={`/assets/images/MetaMask.svg`}
-            className={`h-full object-cover mr-auto relative left-[20px]`}
-          />
-
-          {/* 👇️👇️👇️ MetaMask connection indicator.. */}
-          <FontAwesomeIcon
-            icon={faCheckCircle}
-            className={`absolute top-[1px] right-[-8px] m-2 h-[14px] w-[14px] ${
-              currentAccount_.length != 0 ? "opacity-100" : "opacity-0"
-            } text-blue-500 transition-all duration-[2000ms]`}
-          />
-        </div>
+    // <Router location={""} navigator={navigate}>
+      <div className={`w-full h-[50px] fixed top-0`}>
+        {/* 👇️👇️👇️ MetaMask button.. */}
         <div
-          className={`flex flex-col w-[100px] pl-6 pb-0 relative overflow-hidden transition-all duration-500`}
+          className={`transition-all duration-200 opacity-80 hover:opacity-100 flex flex-row relative right-3 w-[150px] h-full items-center justify-center m-2 ml-auto cursor-pointer`}
+          onClick={() => {
+            connectWallet();
+          }}
         >
-          {/* 👇️👇️👇️ MetaMask Text.. */}
-          <p
-            className={`font-thin relative top-[1px] text-black/50 text-[14px]`}
+          <div className={`w-[40px] h-full transition-all duration-500`}>
+            <img
+              src={`/assets/images/MetaMask.svg`}
+              className={`h-full object-cover mr-auto relative left-[20px]`}
+            />
+
+            {/* 👇️👇️👇️ MetaMask connection indicator.. */}
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className={`absolute top-[1px] right-[-8px] m-2 h-[14px] w-[14px] ${
+                currentAccount_.length != 0 ? "opacity-100" : "opacity-0"
+              } text-blue-500 transition-all duration-[2000ms]`}
+            />
+          </div>
+          <div
+            className={`flex flex-col w-[100px] pl-6 pb-0 relative overflow-hidden transition-all duration-500`}
           >
-            {currentAccount_.length != 0 ? "MetaMask" : "Connect"}
-          </p>
-          <p
-            className={`font-bold relative bottom-1 text-black/50 text-[14px]`}
-          >
-            {currentAccount_.length != 0 ? "Connected" : "MetaMask"}
-          </p>
+            {/* 👇️👇️👇️ MetaMask Text.. */}
+            <p
+              className={`font-thin relative top-[1px] text-black/50 text-[14px]`}
+            >
+              {currentAccount_.length != 0 ? "MetaMask" : "Connect"}
+            </p>
+            <p
+              className={`font-bold relative bottom-1 text-black/50 text-[14px]`}
+            >
+              {currentAccount_.length != 0 ? "Connected" : "MetaMask"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    // </Router>
   );
 };
 
