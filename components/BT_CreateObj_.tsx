51dbe310a5596c146f704268b55e089609971306
LwaziNF <lwazinf@gmail.com>
2022-09-01 21:47:59 +0200

    fix :: uploading function finally working. .Need a progress bar for uploading, warning not to exit site

--- components/BT_CreateObj_.tsx
+++ components/BT_CreateObj_.tsx
@@ -79,6 +79,7 @@ const BT_CreateObj_ = ({}: BT_CreateObj_Props) => {
   const storeImage = async (image: Blob | Uint8Array | ArrayBuffer) => {
     return new Promise((resolve, reject) => {
       const storage = getStorage();
+      // @ts-ignore
       const fileName = `${image.name}`;
 
       const storageRef = ref(storage, "images/" + fileName);
@@ -90,7 +91,7 @@ const BT_CreateObj_ = ({}: BT_CreateObj_Props) => {
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
+          // console.log("Upload is " + progress + "% done");
           switch (snapshot.state) {
             case "paused":
               console.log("Upload is paused");
@@ -111,10 +112,7 @@ const BT_CreateObj_ = ({}: BT_CreateObj_Props) => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
             resolve(downloadURL);
             let files = media_;
-            console.log('////////////////', downloadURL)
             files.push(downloadURL);
-            // setMedia_(files)
-            console.log(files)
           });
         }
       );
@@ -161,7 +159,6 @@ const BT_CreateObj_ = ({}: BT_CreateObj_Props) => {
               await storeImage(imageUpload_[i]);
             }
           }
-          console.log(media_.length);
 
           const x_ = {
             who: {
@@ -184,7 +181,9 @@ const BT_CreateObj_ = ({}: BT_CreateObj_Props) => {
           };
 
           const run_ = await setDoc(doc(db, "campaigns", uuid_), x_);
-          console.log(x_)
+
+          // make a loading animation for uploading. Switch here!
+
           setImageUpload_([])
           setMedia_([])
 
