92783514775a9ac8f64165a965596e3e98bbda56
LwaziNF <lwazinf@gmail.com>
2023-05-05 00:09:57 +0200

    moved app to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,39 @@
-import type { NextPage } from "next";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { db } from "../components/services/firebase";
+import BT_CampaignObj_ from "../components/BT_CampaignObj_";
+import BT_CreateObj_ from "../components/BT_CreateObj_";
+import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+interface BirdTool_Props {}
+
+const BirdTool_ = ({}: BirdTool_Props) => {
+  const [campaigns_, setCampaigns_] = useState<string[]>([]);
+
+  onSnapshot(collection(db, "campaigns"), (snapshot) => {
+    let campaigns : any[] = [];
+    snapshot.docs.forEach((doc) => {
+      return campaigns.push({ ...doc.data() });
+    })
+    setCampaigns_(campaigns)
+  })
+
   return (
-    <div className="flex flex-col items-center justify-center lg:w-[1200px] md:w-[600px] min-h-screen relative">
-      
+    <div className="flex flex-col items-center justify-center w-[1200px] min-h-screen relative">
+      <BT_CreateObj_ />
+      {/* @ts-ignore */}
+      {campaigns_.map((myMap, index) => {
+        return (
+          <BT_CampaignObj_ 
+          key={index}
+          data_={
+            // @ts-ignore
+            campaigns_[index]
+          }
+          />
+        );
+      })}
     </div>
   );
 };
 
-export default Home;
+export default BirdTool_;
---
+++ pages/index_.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center lg:w-[1200px] md:w-[600px] min-h-screen relative">
+      
+    </div>
+  );
+};
+
+export default Home;
